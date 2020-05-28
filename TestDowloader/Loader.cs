dc06de3fdf4267c02f8c6d2945c42de54e10417d
vase_ok 1 <vvaseokk@gmail.com>
2020-05-28 08:53:31 +0300

    Добавьте файлы проекта.

---
+++ TestDowloader/Loader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TestDowloader
+{
+    class Loader
+    {
+
+        public void Loaded(string uri, Label l, ProgressBar p, string name)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.OpenRead(uri);
+                string size = (Convert.ToDouble(wc.ResponseHeaders["Content-Length"]) / 1048576).ToString("#.#");
+
+                wc.DownloadProgressChanged += (s, a) =>
+                {
+                    l.Content = $"Размер файла: {size} \n Загружено {a.ProgressPercentage}%({((double)a.BytesReceived / 1048576).ToString("#.#")} Mb)";
+                    p.Value = a.ProgressPercentage;
+                };
+                wc.DownloadFileAsync(new Uri(uri), $@"C:\Users\vaseok\Desktop\{name}.exe");
+            }
+        }
+    }
+}
---
+++ TestDowloader/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestDowloader
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public  void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var Loaded = new Loader();
+
+            if (checkBox1.IsChecked == true)
+            {
+                checkBox2.IsChecked = checkBox3.IsChecked = false;
+                string uri = "https://www.aimp.ru/?do=download.file&id=4";
+                Loaded.Loaded(uri, label1, progressBar1, "Aimp4");
+                
+            }
+            else if(checkBox2.IsChecked == true)
+            {
+                string uri = "https://t1.daumcdn.net/potplayer/PotPlayer/Version/Latest/PotPlayerSetup64.exe";
+                Loaded.Loaded(uri, label1, progressBar1, "PotPlayer");
+            }
+            else if (checkBox3.IsChecked == true)
+            {
+                string uri = "https://download.tenorshare.net/go/ultdata-for-windows_1094.exe";
+                Loaded.Loaded(uri, label1, progressBar1, "PuranFileReovery");
+            }
+
+        }
+    }
+}
